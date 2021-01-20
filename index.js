f986f0b51bf7ff56c53f1456373d1ec63de740c2
BenjaminS <bsabardu@gmail.com>
2021-01-20 23:53:22 +0100

    Init express ejs

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const router = require('./src/router');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(router);
+
+
+
+app.listen(PORT, ()=> {
+    console.log('Listening on port: ' + PORT)
+})
